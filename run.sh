f94b4ad36fbb5b953d21042ae949a0dc6c2ade79
wentingj <wenting.jiang@intel.com>
2017-11-03 11:34:09 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python tf_rnn_benchmarks_cpu_lstm.py -n basic_lstm -i 3 -l 2 -s 1 -b 3
+#python tf_rnn_benchmarks_cpu_lstm.py -n basic_lstm -i 1 -l 1 -s 1 -b 1
+#python tf_rnn_benchmarks_cpu_lstm.py -n lstm -i 150 -l 1024 -s 10 -b 64
